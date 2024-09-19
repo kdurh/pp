91ce3bbe720e5b4ede083075474d250b94737051
kdurh <93518445+kdurh@users.noreply.github.com>
2024-09-19 10:03:04 +0900

    SFDC <=> Azure <=> Target

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 import logging
 from flask import (Flask, request, Response)
 
@@ -7,15 +9,23 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)  # Set the logging level to DEBUG
 logger = logging.getLogger(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST', 'PUT', 'PATCH', 'HEAD' , 'OPTIONS'])
 def proxy():
+    retry_strategy = Retry(
+        total=2,  # 최대 재시도 횟수
+        backoff_factor=1,  # 대기 시간의 배수 (초 단위)
+    )
+    session = requests.Session()
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
     scheme = request.headers.get('X-Forwarded-Proto', 'http')
     target_host = request.headers.get('X-Forwarded-Host')
     
     if not target_host:
         return Response("Missing Forward-Host header", status=400)
     
-    # Build the full URL
+    # full URL 생성
     full_url = f"{scheme}://{target_host}"
 
     excluded_headers = ['host', 'x-forwarded-proto', 'x-forwarded-host', 'x-forwarded-for',
@@ -23,16 +33,18 @@ def proxy():
                             'x-client-ip', 'x-client-port', 'x-site-deployment-id', 'x-appservice-proto',
                             'x-arr-ssl', 'x-arr-log-id', 'client-ip', 'disguised-host', 
                             'was-default-hostname']
-    # Prepare headers: Exclude 'Host' header
+    
+    # set Target Host Header 
     headers = {key: value for key, value in request.headers if key.lower() not in excluded_headers}
     
     for key, value in request.headers:
         logger.debug(f'Request headers key : {key} | value : {value}')
     for key, value in headers.items():
         logger.debug(f'Headers key : {key} | value : {value}')
+
     # Forward the request to the target URL
     try:
-        response = requests.request(
+        response = session.request(
             method=request.method,
             url=full_url,
             headers=headers,
@@ -40,10 +52,10 @@ def proxy():
             allow_redirects=False,
             stream=True
         )
-        # logger.debug(f'Response status code: {response.status_code}')
-        # logger.debug(f'Response headers: {response.headers}')
-        # logger.debug(f'Response body: {response.text[:1000]}')
-        # logger.debug(f'response encoding : {response.encoding}')
+        logger.debug(f'Response status code: {response.status_code}')
+        logger.debug(f'Response headers: {response.headers}')
+        logger.debug(f'Response body: {response.text[:1000]}')
+        logger.debug(f'response encoding : {response.encoding}')
 
         filtered_headers = {k: v for k, v in response.headers.items() 
                             if k.lower() not in ['content-encoding', 'transfer-encoding']}
@@ -53,8 +65,12 @@ def proxy():
             status=response.status_code,
             headers=dict(filtered_headers)
         )
-    except requests.RequestException as e:
+    except requests.exceptions.RequestException as e:
         return Response(f"Error: {e}", status=500)
+    
+@app.route('/test')
+def test():
+    return "Test"
 
 if __name__ == '__main__':
-   app.run()
+   app.run(port=8080)
